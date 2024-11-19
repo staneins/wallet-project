52cbf021365ff0b1e7d80a5aacac33583b48052d
Philip Kaminsky <kaminsky8836@gmail.com>
2024-11-19 13:35:19 +0300

    initial commit

---
+++ src/test/java/com/kaminsky/walletproject/WalletProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.walletproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
